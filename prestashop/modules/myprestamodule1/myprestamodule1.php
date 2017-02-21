7a3ee8501bee82ee7d71f532c3c39d14b65ffe0b
laidet_r <rodolphe.laidet@epitech.eu>
2017-02-21 09:11:25 +0100

    [first-module-begin] Update the Module file of the first module, authors and desc [#2]

--- prestashop/modules/myprestamodule1/myprestamodule1.php
+++ prestashop/modules/myprestamodule1/myprestamodule1.php
@@ -8,9 +8,9 @@ class MyPrestaModule1 extends Module
   public function __construct()
   {
     $this->name = 'myprestamodule1';
-    $this->tab = 'social_networks';
+    $this->tab = 'front_office_features';
     $this->version = '1.0.0';
-    $this->author = 'Rodolphe Laidet';
+    $this->author = 'laidet_r & cherbi_r';
     $this->need_instance = 0;
     $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
     $this->bootstrap = true;
@@ -18,9 +18,9 @@ class MyPrestaModule1 extends Module
     parent::__construct();
 
     $this->displayName = $this->l('My PrestaModule 1');
-    $this->description = $this->l('A kick-ass first presta module.');
+    $this->description = $this->l('A kick-ass first presta module to add a Youtube video on a product page.');
 
-    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+    $this->confirmUninstall = $this->l('Are you sure you want to uninstall ?!');
 
     if (!Configuration::get('MYMODULE_NAME')) {
       $this->warning = $this->l('No name provided');
@@ -29,8 +29,7 @@ class MyPrestaModule1 extends Module
 
   public function install()
   {
-    if(!parent::install() ||
-      !Configuration::updateValue("MY_PRESTAMODULE_1_NAME", "toto")) {
+    if(!parent::install()) {
       return false;
     }
     return true;
@@ -38,8 +37,7 @@ class MyPrestaModule1 extends Module
 
   public function uninstall()
   {
-    if(!parent::uninstall() ||
-      !Configuration::deleteByName("MY_PRESTAMODULE_1_NAME")) {
+    if(!parent::uninstall()) {
       return false;
     }
     return true;
