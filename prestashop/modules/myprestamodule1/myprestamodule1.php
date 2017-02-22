12f383f6644ecf2edcbfe76e2caf4ddf6df6ffbd
Rodolphe Laidet <rodolphe.laidet@habiteo.com>
2017-02-22 22:57:35 +0100

    [first-module-begin] Add a tab in AdminProducts with helperform... Soooo ugly ! [#2]

--- prestashop/modules/myprestamodule1/myprestamodule1.php
+++ prestashop/modules/myprestamodule1/myprestamodule1.php
@@ -87,7 +87,76 @@ class MyPrestaModule1 extends Module
 
   public function hookDisplayAdminProductsExtra($params)
   {
-    return $this->display(__FILE__, "display-product-admin-extra.tpl");
+    $id_product = (int)Tools::getValue('id_product');
+    $video = Video::findByProductId($id_product);
+
+    // Get default language
+    $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
+
+    // Init Fields form array
+    $fields_form[0]['form'] = array(
+        'legend' => array(
+            'title' => $this->l('Youtube video'),
+        ),
+        'input' => array(
+            array(
+                'type' => 'text',
+                'label' => $this->l('Video URL'),
+                'desc' => "The URL to your Youtube video",
+                'name' => 'YOUTUBE_URL',
+                'hint' => 'https://www.youtube.com/watch?v=XXXXXXXXX',
+                'required' => false
+            )
+        ),
+        'submit' => array(
+            'title' => $this->l('Save'),
+            'class' => 'btn btn-default pull-right'
+        ),
+        'submitAddproductAndStay' => array(
+            'title' => $this->l('Save and stay'),
+            'class' => 'btn btn-default pull-right'
+        )
+    );
+
+    $helper = new HelperForm();
+
+    // Module, token and currentIndex
+    $helper->module = $this;
+    $helper->name_controller = $this->name;
+    $helper->token = Tools::getAdminTokenLite('AdminProducts');
+    $helper->currentIndex = AdminController::$currentIndex.'&id_product='.$id_product.'&updateproduct';
+
+    // Language
+    $helper->default_form_language = $default_lang;
+    $helper->allow_employee_form_lang = $default_lang;
+
+    // Title and toolbar
+    $helper->title = $this->displayName;
+    $helper->show_toolbar = true;        // false -> remove toolbar
+    $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+    $helper->submit_action = 'submitAddproduct';
+    $helper->toolbar_btn = array(
+        'save' =>
+        array(
+            'desc' => $this->l('Save'),
+            'href' => AdminController::$currentIndex.'&id_product='.$id_product.'&updateproduct'.
+            '&token='.Tools::getAdminTokenLite('AdminProducts'),
+        ),
+        'back' => array(
+            'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminProducts'),
+            'desc' => $this->l('Back to list')
+        )
+    );
+
+    // Load current value
+    $helper->fields_value['YOUTUBE_URL'] = $video ? 'https://www.youtube.com/watch?v=' . $video->key : '';
+
+    $form = $helper->generateForm($fields_form);
+    $regex = '/<form.[^>]*>/';
+    $formContent = preg_replace($regex, '', $form);
+    $formContent = preg_replace('</form>', '', $formContent);
+
+    return $formContent;
   }
 
   public function hookActionProductUpdate($params)
